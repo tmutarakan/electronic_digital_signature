1c6b98646afa7e69cd69c8956d68b4ce82569978
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2025-05-24 17:58:53 +0300

    redesign displaying data in the admin

--- backend/api/core/admin.py
+++ backend/api/core/admin.py
@@ -1,8 +1,53 @@
 from django.contrib import admin
-from .models import Organization, Position, CivilDocument, Employee, ElectronicDigitalSignature
+from .models import (
+    Organization,
+    Position,
+    CivilDocument,
+    Employee,
+    ElectronicDigitalSignature,
+)
 
-admin.site.register(Organization)
-admin.site.register(Position)
-admin.site.register(CivilDocument)
-admin.site.register(Employee)
-admin.site.register(ElectronicDigitalSignature)
+
+class SettingAdminOrganization(admin.ModelAdmin):
+    list_display = ("title", "ogrn", "inn", "kpp", "registered_address")
+
+
+class SettingAdminPosition(admin.ModelAdmin):
+    list_display = ("title", "organization")
+
+
+class SettingAdminCivilDocument(admin.ModelAdmin):
+    list_display = (
+        "series",
+        "number",
+        "date_of_issue",
+        "birthdate",
+        "birthplace",
+        "code",
+        "inn",
+        "snils",
+    )
+
+
+class SettingAdminEmployee(admin.ModelAdmin):
+    list_display = (
+        "surname",
+        "name",
+        "patronymic",
+        "gender",
+        "position",
+        "civil_document",
+        "slug",
+        "created_at",
+    )
+
+
+class SettingAdminElectronicDigitalSignature(admin.ModelAdmin):
+    list_display = ("title", "sertificate", "archive", "owner")
+
+
+admin.site.register(Organization, SettingAdminOrganization)
+admin.site.register(Position, SettingAdminPosition)
+admin.site.register(CivilDocument, SettingAdminCivilDocument)
+admin.site.register(Employee, SettingAdminEmployee)
+admin.site.register(ElectronicDigitalSignature, SettingAdminElectronicDigitalSignature)
--- backend/api/core/models.py
+++ backend/api/core/models.py
@@ -5,13 +5,14 @@ from .validators import validate_file_sertificate, validate_file_archive
 
 
 class Organization(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name="Название")
     ogrn = models.CharField(
         max_length=13,
         validators=[
             MinLengthValidator(13),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="ОГРН",
     )  # Основной государственный регистрационный номер
     inn = models.CharField(
         max_length=10,
@@ -19,6 +20,7 @@ class Organization(models.Model):
             MinLengthValidator(10),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="ИНН",
     )  # Идентификационный номер налогоплательщика
     kpp = models.CharField(
         max_length=9,
@@ -26,8 +28,11 @@ class Organization(models.Model):
             MinLengthValidator(9),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="КПП",
     )  # Код причины постановки на учёт
-    registered_address = models.CharField(max_length=200)
+    registered_address = models.CharField(
+        max_length=200, verbose_name="Юридический адрес"
+    )
 
     def __str__(self):
         return str(self.title)
@@ -38,8 +43,10 @@ class Organization(models.Model):
 
 
 class Position(models.Model):
-    title = models.CharField(max_length=200)
-    organization = models.ForeignKey(Organization, on_delete=models.PROTECT)
+    title = models.CharField(max_length=200, verbose_name="Название")
+    organization = models.ForeignKey(
+        Organization, on_delete=models.PROTECT, verbose_name="Организация"
+    )
 
     def __str__(self):
         return str(self.title)
@@ -56,6 +63,7 @@ class CivilDocument(models.Model):
             MinLengthValidator(4),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="Серия",
     )
     number = models.CharField(
         max_length=6,
@@ -63,16 +71,18 @@ class CivilDocument(models.Model):
             MinLengthValidator(6),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="Номер",
     )
-    date_of_issue = models.DateField()
-    birthdate = models.DateField()
-    birthplace = models.CharField(max_length=200)
+    date_of_issue = models.DateField(verbose_name="Дата выдачи")
+    birthdate = models.DateField(verbose_name="Дата рождения")
+    birthplace = models.CharField(max_length=200, verbose_name="Место рождения")
     code = models.CharField(
         max_length=7,
         validators=[
             MinLengthValidator(7),
             RegexValidator(regex=r"^\d{3}-\d{3}$", message="Формат: 000-000"),
         ],
+        verbose_name="Код подразделения",
     )  # Код подразделения
     inn = models.CharField(
         max_length=12,
@@ -80,6 +90,7 @@ class CivilDocument(models.Model):
             MinLengthValidator(12),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="ИНН",
     )
     snils = models.CharField(
         max_length=11,
@@ -87,6 +98,7 @@ class CivilDocument(models.Model):
             MinLengthValidator(11),
             RegexValidator(regex=r"^\d+$", message="Разрешены только цифры."),
         ],
+        verbose_name="СНИЛС",
     )  # Страховой номер индивидуального лицевого счёта
 
     def __str__(self):
@@ -102,14 +114,22 @@ class Employee(models.Model):
         ("М", "Мужской"),
         ("Ж", "Женский"),
     ]
-    surname = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    patronymic = models.CharField(max_length=200)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    position = models.ForeignKey(Position, on_delete=models.PROTECT)
-    civil_document = models.OneToOneField(CivilDocument, on_delete=models.PROTECT)
+    surname = models.CharField(max_length=200, verbose_name="Фамилия")
+    name = models.CharField(max_length=200, verbose_name="Имя")
+    patronymic = models.CharField(max_length=200, verbose_name="Отчество")
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, verbose_name="Пол")
+    position = models.ForeignKey(
+        Position, on_delete=models.PROTECT, verbose_name="Должность"
+    )
+    civil_document = models.OneToOneField(
+        CivilDocument,
+        on_delete=models.PROTECT,
+        verbose_name="Документы",
+        null=True,
+        blank=True,
+    )
     slug = models.SlugField()
-    created_at = models.DateField(default=timezone.now)
+    created_at = models.DateField(default=timezone.now, verbose_name="Дата добавления")
 
     def __str__(self):
         return f"{self.surname} {self.name} {self.patronymic}"
@@ -120,10 +140,18 @@ class Employee(models.Model):
 
 
 class ElectronicDigitalSignature(models.Model):
-    title = models.CharField(max_length=200)
-    sertificate = models.FileField(validators=[validate_file_sertificate])
-    archive = models.FileField(validators=[validate_file_archive])
-    owner = models.ForeignKey(Employee, on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=200, verbose_name="Имя файла")
+    sertificate = models.FileField(
+        validators=[validate_file_sertificate], verbose_name="Сертификат"
+    )
+    archive = models.FileField(validators=[validate_file_archive], verbose_name="Архив")
+    owner = models.ForeignKey(
+        Employee,
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name="Владелец",
+    )
 
     class Meta:
         verbose_name = "ЭЦП"
